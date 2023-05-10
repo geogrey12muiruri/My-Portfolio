9aa0e571f2d5c56f9f65d2d5bbffd6cc9005f1b6
geogrey12muiruri <greggambugua@gmail.com>
2023-05-10 18:01:20 +0300

    minor changes on the logo icon

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -30,46 +30,49 @@ const Logo = () => {
         viewBox="0 0 120 120"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <circle cx="60" cy="40" r="20" fill="#ED1C24" />
-        <rect x="45" y="60" width="30" height="30" fill="#000" />
+        <rect x="30" y="25" width="60" height="50" fill="#000" />
+        <rect x="35" y="30" width="50" height="40" fill="#00FF00" />
+        <rect x="45" y="35" width="30" height="30" fill="#000" />
+        <rect x="38" y="45" width="12" height="10" fill="#000" />
+        <rect x="70" y="45" width="12" height="10" fill="#000" />
         <path d="M60,80 C55,85 65,85 60,80 L60,80 Z" fill="#FFF" />
         <path d="M50,50 Q60,45 70,50" fill="none" stroke="#000" strokeWidth="2" />
         <path d="M50,55 Q60,60 70,55" fill="none" stroke="#000" strokeWidth="2" />
         <path d="M50,60 Q60,65 70,60" fill="none" stroke="#000" strokeWidth="2" />
-        <g id="coder-body" transform="translate(60, 47)">
-          <rect x="-15" y="-10" width="30" height="20" fill="#fff" />
-          <rect x="-13" y="-8" width="26" height="16" fill="#fff" />
-          <rect x="-11" y="-6" width="22" height="12" fill="#fff" />
+        <g id="hacker-head" transform="translate(60, 25)">
+          <circle cx="0" cy="0" r="10" fill="#00FF00" />
+          <path d="M-7,-7 Q0,-10 7,-7" fill="none" stroke="#000" strokeWidth="2" />
+          <path d="M-6,-6 Q0,-9 6,-6" fill="none" stroke="#000" strokeWidth="2" />
+          <path d="M-5,-5 Q0,-8 5,-5" fill="none" stroke="#000" strokeWidth="2" />
         </g>
-        <g id="coder-arm-left" transform="translate(40, 50)">
-          <rect x="-10" y="-2" width="10" height="20" fill="#fff" />
-          <rect x="-9" y="-1" width="8" height="18" fill="#000" />
-          <rect x="-6" y="-8" width="15" height="6" fill="#000" />
+        <g id="hacker-torso" transform="translate(60, 45)">
+          <rect x="-15" y="-10" width="30" height="20" fill="#00FF00" />
+          <rect x="-13" y="-8" width="26" height="16" fill="#00FF00" />
+          <rect x="-11" y="-6" width="22" height="12" fill="#00FF00" />
         </g>
-        <g id="coder-arm-right" transform="translate(80, 50)">
-          <rect x="0" y="-2" width="10" height="20" fill="#fff" />
-          <rect x="1" y="-1" width="8" height="18" fill="#000" />
-          <rect x="-9" y="-8" width="15" height="6" fill="#000" />
+        <g id="hacker-arm-left" transform="translate(35, 50)">
+          <rect x="-10" y="-2" width="10" height="20" fill="#00FF00" />
+          <rect x="-8" y="0" width="6" height="16" fill="#00FF00" />
+          <rect x="-6" y="2" width="2" height="12" fill="#00FF00" />
         </g>
-        <g id="coder-leg-left" transform="translate(50, 70)">
-          <rect x="-5" y="-2" width="10" height="20" fill="#fff" />
-          <rect x="-4" y="-1" width="8" height="18" fill="#000" />
-          <rect x="-1" y="-8" width="15" height="6" fill="#000" />
+        <g id="hacker-arm-right" transform="translate(85, 50)">
+          <rect x="0" y="-2" width="10" height="20" fill="#00FF00" />
+          <rect x="2" y="0" width="6" height="16" fill="#00FF00" />
+          <rect x="4" y="2" width="2" height="12" fill="#00FF00" />
         </g>
-        <g id="coder-leg-right" transform="translate(70, 70)">
-          <rect x="0" y="-2" width="10" height="20" fill="#fff" />
-          <rect x="1" y="-1" width="8" height="18" fill="#000" />
-          <rect x="-9" y="-8" width="15" height="6" fill="#000" />
+        <g id="hacker-leg-left" transform="translate(50, 70)">
+          <rect x="-5" y="-2" width="10" height="20" fill="#00FF00" />
+          <rect x="-3" y="0" width="6" height="16" fill="#00FF00" />
+          <rect x="-1" y="2" width="2" height="12" fill="#00FF00" />
         </g>
-        <g id="coder-head" transform="translate(60, 30)">
-          <circle cx="0" cy="0" r="10" fill="#fff" />
-          <circle cx="0" cy="0" r="8" fill="#fff" />
-          <circle cx="0" cy="0" r="6" fill="#fff" />
-          <circle cx="0" cy="0" r="4" fill="#fff" />
-          <circle cx="0" cy="0" r="2" fill="#fff" />
+        <g id="hacker-leg-right" transform="translate(70, 70)">
+          <rect x="-5" y="-2" width="10" height="20" fill="#00FF00" />
+          <rect x="-3" y="0" width="6" height="16" fill="#00FF00" />
+          <rect x="-1" y="2" width="2" height="12" fill="#00FF00" />
         </g>
       </svg>
     </div>
   );
 };
 export default Logo;
+          
